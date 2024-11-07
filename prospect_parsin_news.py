935cac8707dcae605e76175db8b5df8a465a22c9
Gleb <gleb.bortsov79@gmail.com>
2024-11-07 14:30:31 +1000

    Development:
    * parser

---
+++ prospect_parsin_news.py
@@ -0,0 +1,32 @@
+# 7891535798:AAGUtwq5OScPeG6pPP0j3IVx4EQDCA9SvC8 - bot ID
+# !pip install pyTelegramBotAPI - скачивание API бота в Google Colab
+# <tg-spoiler> - скрывает сообщения, но используется parse_mode='html'
+
+import telebot
+
+from telebot import types
+
+isTest = False
+version = 'v0.1.2 - создание парсера'
+
+token = '7891535798:AAGUtwq5OScPeG6pPP0j3IVx4EQDCA9SvC8'
+
+parsingBot = telebot.TeleBot(token)
+
+@parsingBot.message_handler(commands=['start'])
+def startBot(message):
+    nameUser = message.from_user.first_name
+
+    mess = f"Привет, {nameUser}!\n\nДавай напишем крутые новости!"
+    markup = types.InlineKeyboardMarkup()
+    setNewsButton = types.InlineKeyboardButton("Получить новости", callback_data= "set_news")
+    markup.add(setNewsButton)
+    parsingBot.send_message(message.chat.id, mess, parse_mode= "Markdown", reply_markup=markup)
+
+@parsingBot.callback_query_handler(func=lambda call: True)
+def messageBot(callback):
+    if callback.data == "set_news":
+        mess = "Функция в работе"
+        parsingBot.send_message(callback.message.chat.id, mess, parse_mode="Markdown", reply_markup=None)
+
+parsingBot.infinity_polling()
